6f7127a9689c95496566392f6371f82e8d88d439
Luís Henrique <luhen7765@gmail.com>
2025-12-25 23:28:22 -0300

    Inicio

---
+++ IniciodeJava/src/main/java/olamundo/IniciodeJava.java
@@ -0,0 +1,8 @@
+package olamundo;
+public class IniciodeJava {
+
+    public static void main(String[] args) {
+        System.out.println("Ola, Mundo!");
+        System.out.println("It Starts Here");
+    }
+}
